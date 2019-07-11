96cae13bfbc2160fa87a4e0e82a599c91004cd6f
Amaris Merino <51328772+afmerino@users.noreply.github.com>
2019-07-11 15:23:18 -0400

    Add files via upload

---
+++ week 4/changeelements.js
@@ -0,0 +1,11 @@
+"use strict"
+window.onload = function () {
+var heading1 = document.getElementById("p1"); 
+heading1.style.color = '#ff0000'; 
+
+var heading2 = document.getElementById("p2");
+heading2.style.color = '#cc00ff';
+
+var heading3 = document.getElementById("p3");
+heading3.style.color = '#0000ff';
+}
